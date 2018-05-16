8372b3668a72bc6bd062f3e6c3c6524fbdda4669
zxd <izayoi5776@gmail.com>
2018-05-16 21:47:40 +0900

    同時に打楽器音も合成
    一様分布使ってbの確率を実現。

--- synthesis_Karplus_Strong.m
+++ synthesis_Karplus_Strong.m
@@ -4,22 +4,32 @@
 fs = 44100;     % sample rate
 p = 32;        % buffer size  pitch at fs/(p+1/2)= 44100/512.5=86.05Hz
 
+b = 0.5;        % 0.2=>metal, 0.5=>snare, 0.8=>metal
+pd = makedist('Uniform');
+
 % init 1 second sample
 y = zeros(fs,1);
+z = zeros(fs,1);
 
 % set 1st buffer as random
 for i =1:p+1
     y(i) = rand();
+    z(i) = y(i);
 end
 
 % Karplus-Strong
 for i = p+2:fs
-  y(i) = (y(i-p) + y(i-p-1)) / 2; 
+  y(i) = (y(i-p) + y(i-p-1)) / 2;
+  if random(pd) > b
+      z(i) = (z(i-p) + z(i-p-1)) / 2;
+  else
+      z(i) = -1 * (z(i-p) + z(i-p-1)) / 2;
+  end
 end
 
-s1 = call_dsp_TimeScope(fs, y);
-s2 = call_dsp_SpectrumAnalyzer(fs,y);
-s3 = call_play_sound(fs,y);
+s1 = call_dsp_TimeScope(fs, y, z);
+s2 = call_dsp_SpectrumAnalyzer(fs,y,z);
+%s3 = call_play_sound(fs,y);
 
 
 %% 
@@ -27,18 +37,18 @@ s3 = call_play_sound(fs,y);
 % 
 % common functions
 %%
-function scope = call_dsp_TimeScope(fs, y)
-    scope = dsp.TimeScope('SampleRate',fs,'TimeSpan',1);
+function scope = call_dsp_TimeScope(fs, y, z)
+    scope = dsp.TimeScope('SampleRate',fs,'TimeSpan',1,'NumInputPorts',2);
     hide(scope);
-    scope(y);
+    scope(y,z);
     show(scope);
     release(scope);
 end
 
-function scope = call_dsp_SpectrumAnalyzer(fs, y)
-    scope = dsp.SpectrumAnalyzer('SpectrumType','RMS', 'SampleRate', fs, 'PlotAsTwoSidedSpectrum',false);
+function scope = call_dsp_SpectrumAnalyzer(fs, y,z)
+    scope = dsp.SpectrumAnalyzer('SpectrumType','RMS', 'SampleRate', fs, 'PlotAsTwoSidedSpectrum',false,'NumInputPorts',2);
     hide(scope);
-    scope(y);
+    scope(y,z);
     show(scope);
     release(scope);
 end
